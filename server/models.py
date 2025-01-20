6200ff1f28670f7f46443d11f362b17f983e2774
Oliver cheruiyot <olivercher1000@gmail.com>
2025-01-20 23:39:45 +0300

    Define Plant model with name, image, and price columns

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.types import Numeric
 
 db = SQLAlchemy()
 
@@ -7,3 +8,17 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
+    price = db.Column(db.Numeric(10, 2), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'price': f"{self.price:.2f}"
+        }
+
+    def __repr__(self):
+        return f"<Plant {self.name}>"
